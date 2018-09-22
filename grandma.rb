c8b79202990a13d4df360d58c3fb92d5f9ee3dbd
Cara Morelli <caramorelli7@gmail.com>
2018-09-22 23:18:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -23,5 +23,4 @@ def speak_to_grandma(string)
   return 'I LOVE YOU TOO PUMPKIN!' if string == 'I LOVE YOU GRANDMA!'
   return 'HUH?! SPEAK UP, SONNY!' if string != string.upcase
   'NO, NOT SINCE 1938!'
-  
 end 
